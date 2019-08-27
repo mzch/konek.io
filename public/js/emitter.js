39cb8a255f70f0f036cdb9a094a429d5c24e925c
justinemar <justinecantado@gmail.com>
2019-08-27 12:31:55 +0800

    tweaked

--- public/js/emitter.js
+++ public/js/emitter.js
@@ -29,11 +29,14 @@
                     const column = document.createElement('div');
                     column.classList.add('one-half', 'columns', 'messages');
                     const h3 = document.createElement('p');
-                    h3.innerText = text.data.TextArray[text.data.TextArray.length - 1]
-                    h3.setAttribute('id', text.data.TextArray[text.data.TextArray.length - 1].substring(1, 10))
+               
+                    h3.innerText = text.data.TextArray[text.data.TextArray.length - 1].text
+                    h3.setAttribute('id', text.data.TextArray[text.data.TextArray.length - 1]._id.substring(1, 10))
+   
                     const span = document.createElement('span');
                     span.classList.add('clip')
-                    span.setAttribute('data-clipboard-target', `#${text.data.TextArray[text.data.TextArray.length - 1].substring(1, 10)}`)
+                    span.setAttribute('data-clipboard-target', `#${text.data.TextArray[text.data.TextArray.length - 1]._id.substring(1, 10)}`)
+                                               
                     row.appendChild(column);
                     column.appendChild(h3)
                     column.appendChild(span)

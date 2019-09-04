fac8ab1f905bbd68eb5e1ea4960a8fa258a1d2d8
justinemar <justinecantado@gmail.com>
2019-09-04 13:20:49 +0800

    remove unnecessary test

--- test/config.spec.js
+++ test/config.spec.js
@@ -4,6 +4,7 @@ const db = require('../lib/frameworks_drivers/database');
 require("dotenv").config();
 
 describe('test configs', function () {
+
     describe('process.env required keys', function () {
 
         it('should have DB key', function () {
@@ -12,40 +13,6 @@ describe('test configs', function () {
 
         it('should have DB_PASS key', function () {
             expect(process.env).to.have.own.property('DB_PASS');
-        })
-    })
-
-    describe('#database()', function () {
-
-        context('with required keys', function () {
-            it('should not return database connection error', function (done) {
-                const config = {
-                    username: process.env.DB,
-                    password: process.env.DB_PASS
-                }
-
-                expect(function () {
-
-                    db.config(config)
-                    done()
-                }).to.throw(TypeError, 'Unable to connect to database.')
-              })
-        })
-
-        context('without required keys', function () {
-            it('should return database connection error', function (done) {
-                const config = {
-                    username: process.env.DB,
-                    password: process.env.DB_PASS
-                }
-
-                expect(function () {
-
-                    db.config(config)
-                    done()
-                }).to.throw(TypeError, 'Unable to connect to database.')
-
-            })
         })
     })
 
